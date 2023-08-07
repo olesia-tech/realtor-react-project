4147dcb5d8b622b823fe9f6f8495969fa59da194
olesia-tech <shikotanshop@gmail.com>
2023-08-07 15:50:48 -0400

    completed the header component and made it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import {getAuth, onAuthStateChanged} from 'firebase/auth';
 
 export default function Header() {
+  const [pageState, setPageState] = useState('Sign in');
   const location = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth();
 
-  function pathMathRoute(route){
+  useEffect(() => {
+onAuthStateChanged(auth, (user) => {
+  if(user){
+    setPageState('Profile');
+  }else{
+    setPageState('Sign in');
+  }
+});
+  },[auth]);
+
+  function pathMatchRoute(route){
     if(route === location.pathname){
       return true;
     }
@@ -18,12 +31,26 @@ export default function Header() {
         </div>
         <div>
           <ul className='flex space-x-10'>
-          <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMathRoute('/') ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`}
-           onClick={() => navigate('/')}>Home</li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMathRoute('/offers') ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`}
-            onClick={() => navigate('/offers')}>Offers</li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMathRoute('/sign-in') ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`}
-            onClick={() => navigate('/sign-in')}>Sign in</li>
+          <li 
+          className={`cursor-pointer py-3 text-sm font-semibold ${pathMatchRoute('/') ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`}
+           onClick={() => navigate('/')}>
+            Home
+            </li>
+            <li 
+            className={`cursor-pointer py-3 text-sm font-semibold ${pathMatchRoute('/offers') ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`}
+            onClick={() => navigate('/offers')}>
+              Offers
+              </li>
+            <li 
+            className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${
+              pathMatchRoute('/sign-in') || pathMatchRoute('/profile')
+                ? 'text-black border-b-red-500'
+                : 'text-gray-400 border-b-transparent'
+            }`}
+            onClick={() => navigate('/profile')}
+            >
+              {pageState}
+              </li>
           </ul>
         </div>
       </header>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -43,8 +43,8 @@ try {
   formDataCopy.timestamp = serverTimestamp();
 
   await setDoc(doc(db, 'users', user.uid), formDataCopy);
-  toast.success('Sign up was successful!')
-  navigate('/');
+  //toast.success('Sign up was successful!')
+  //navigate('/');
 } catch (error) {
   toast.error('Something went wrong with the registration');
 }
